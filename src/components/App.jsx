b2d1f3d5de3b881a3b575400c09ee993fa9653da
Moad Boutaj <boutajmouad@proton.me>
2026-05-08 15:31:06 +0200

    Update fetch URL to use CORS proxy
    For testing & demo purposes only

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 
   useEffect(() => {
 
-    fetch(`https://ipwho.is/${ip}`)
+    fetch(`https://corsproxy.io/?url=${encodeURIComponent("https://ipwho.is/" + ip)}`)
     .then(res => res.json())
     .then(data => {
       
